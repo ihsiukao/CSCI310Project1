1dab8033c55be9decbf66a97a5d6123c4c04bdc0
uscitp-ihsiukao <ihsiukao@usc.edu>
2018-09-09 22:33:32 -0700

    Added File I/O funtion

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -3,9 +3,58 @@ import java.util.*;
 
 public class FlightMap {
 	private Map<String, Airport> airMap;
+	FileReader fr;
+	BufferedReader br;
 	
-	public FlightMap() {
+	public FlightMap(String inputName) {
 		airMap = new HashMap<String, Airport>();
+		constructMap(inputName);
+	}
+	private void constructMap(String inputName) {
+		try {
+			fr = new FileReader(inputName);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			Airport temp = new Airport(line);
+			airMap.put(line, temp);
+			while(line != null) {
+				line = br.readLine();
+				String[] words = line.split("\\s+");
+				if(airMap.containsKey(words[0])) {
+					temp = airMap.get(words[0]);
+					temp.addFlight(words[1], Integer.parseInt(words[2]));
+					airMap.remove(words[0]);
+				}
+				else 
+				{
+					temp = new Airport(words[0]);
+				}
+				airMap.put(words[0], temp);
+				if(!airMap.containsKey(words[1])) {
+					temp = new Airport(words[1]);
+					airMap.put(words[1], temp);
+				}
+			}
+		} catch (FileNotFoundException fnfe) {
+			System.out.println(fnfe.getMessage());
+		} catch (IOException ioe) {
+			System.out.println(ioe.getMessage());
+		} finally {
+			if(br != null) {
+				try {
+					br.close();
+				} catch (IOException ioe) {
+					System.out.print(ioe.getMessage());
+				}
+			}
+			if(fr != null) {
+				try {
+					fr.close();
+				} catch (IOException ioe) {
+					System.out.print(ioe.getMessage());
+				}
+			}
+		}
 	}
 	public void addAirport(String key, Airport air) {
 		airMap.put(key, air);
--- src/SearchMap.java
+++ src/SearchMap.java
@@ -4,24 +4,10 @@ import java.util.*;
 public class SearchMap {
 	public static void main(String[] args) {
 		String inputName;
-		FileReader fr;
-		BufferedReader br;
 		FlightMap map;
 		inputName = args[0];
-		map = new FlightMap();
-		try {
-			fr = new FileReader(inputName);
-			br = new BufferedReader(fr);
-			map = constructMap(br);
-		}
+		map = new FlightMap(inputName);
 
-	}
-	public static FlightMap constructMap(BufferedReader br) {
-		FlightMap map = new FlightMap();
-		
-	}
-	public Map<List<String>, Integer> findRoute() {
-		
 	}
 
 }
