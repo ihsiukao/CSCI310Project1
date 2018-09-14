f0c5e9e19310a1ad51c89ca6851629e4bd88ccf3
uscitp-ihsiukao <ihsiukao@usc.edu>
2018-09-13 20:38:33 -0700

    Added graph search funtionality

--- src/Airport.java
+++ src/Airport.java
@@ -8,11 +8,18 @@ public class Airport {
 	
 	public Airport(String name) {
 		airName = name;
+		directFlight = new HashMap<String, Integer>();
 	}
 	public String getName() {
 		return airName;
 	}
 	public void addFlight(String name, int cost) {
 		directFlight.put(name, cost);
+	}
+	public int numberFlight() {
+		return directFlight.size();
+	}
+	public Map<String, Integer> getFlight(){
+		return directFlight;
 	}
 }
--- src/SearchMap.java
+++ src/SearchMap.java
@@ -3,11 +3,12 @@ import java.util.*;
 
 public class SearchMap {
 	public static void main(String[] args) {
-		String inputName = args[0];
-		String outputName = args[1];
+		//String inputName = args[0];
+		//String outputName = args[1];
 		FlightMap map;
-		map = new FlightMap(inputName);
-		map.findRoutes(outputName);
+		map = new FlightMap("inputfile.txt");
+		map.writeFile("outputfile.txt");
+		return;
 	}
 
 }
